6c35426b29cd0886bc9443b24f1f156bec7e3756
Xiangnan Dang <dang_xiangnan@lilly.com>
2020-05-14 01:17:59 -0400

    well, it seems scrolling in a tabpanel and fixedheader do not play well together

--- R/esquisserUI.R
+++ R/esquisserUI.R
@@ -134,7 +134,7 @@ esquisserUI <- function(id, header = TRUE,
               shiny::tabPanel(
                 title = "datatable",
                 tags$div(
-                  style = "width:100%;overflow-x:scroll",
+                  style = "height:800px;width:100%;overflow-x:scroll;overflow-y:scroll",
                   DT::DTOutput(ns("datatable"))
                 )
               )
